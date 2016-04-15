16fd84be05bd015570a46b5ee5079178f184d473
thenewjeva <tdowey3@vt.edu>
2016-04-15 12:34:52 -0400

    Added video playback to application

--- TherapistApp/src/application/Main.java
+++ TherapistApp/src/application/Main.java
@@ -6,7 +6,9 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 import javafx.stage.FileChooser;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -19,57 +21,30 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 import javafx.geometry.Insets;
 
 
 public class Main extends Application {
 	private Model model;
-	private Media media;
-	private MediaPlayer mediaPlayer;
-	private MediaView mediaView;
-	private StackPane mediaPane;
 	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,1024,768);
+			Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+			Scene scene = new Scene(root, primaryScreenBounds.getWidth(), primaryScreenBounds.getHeight());
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			model = new Model();
-			HBox analysisView = new HBox(), commentView = new HBox();
-			
-			//Video View
-			
-			/*Placing Video (Needs Video Selected before Uploading
-			mediaPane = new StackPane();
-			mediaPane.setId("mediaPane");
-			media = new Media(model.videoPath);
-			mediaPlayer = new MediaPlayer(media);
-			mediaPlayer.setAutoPlay(true);
-			mediaView = new MediaView(mediaPlayer);
-			mediaView.setPreserveRatio(true);
-			mediaPane.getChildren().clear();
-			mediaPane.getChildren().add(mediaView);
-			HBox hbVideo = new HBox();
-			hbVideo.setStyle("-fx-background-color: black;");
-			hbVideo.setAlignment(Pos.CENTER);
-			hbVideo.getChildren().add(mediaPane);
-			root.setLeft(hbVideo);*/
 			
 			//All code at this point creates the Fidelity View
-			GridPane grid = new GridPane();
-			grid.setAlignment(Pos.CENTER);
-	        grid.setHgap(10);
-	        grid.setVgap(10);
-	        grid.setScaleShape(true);
-	        grid.setPadding(new Insets(25, 25, 25, 25));
+			GridPane analysisView = new GridPane();
+			analysisView.setAlignment(Pos.CENTER);
+	        analysisView.setHgap(10);
+	        analysisView.setVgap(10);
+	        analysisView.setScaleShape(true);
+	        analysisView.setPadding(new Insets(25, 25, 25, 25));
 	        
 	        HBox childAttn = new HBox();
 	        childAttn.setAlignment(Pos.TOP_RIGHT);
@@ -99,71 +74,83 @@ public class Main extends Application {
 	        attempts.setAlignment(Pos.TOP_RIGHT);
 	        attempts.getChildren().add(new Label("Attempts:"));
 	        
-	        //adds HBoxes to the grid
-	        grid.add(childAttn, 0, 0);
-	        grid.add(clearOpp, 0, 1);
-	        grid.add(total, 0, 2);
-	        grid.add(maintenance, 0, 3);
-	        grid.add(childChoice, 0, 4);
-	        grid.add(sharedControl, 0, 5);
-	        grid.add(contingent, 0, 6);
-	        grid.add(natural, 0, 7);
-	        grid.add(attempts, 0, 8);
+	        //adds HBoxes to the analysisView
+	        analysisView.add(childAttn, 0, 0);
+	        analysisView.add(clearOpp, 0, 1);
+	        analysisView.add(total, 0, 2);
+	        analysisView.add(maintenance, 0, 3);
+	        analysisView.add(childChoice, 0, 4);
+	        analysisView.add(sharedControl, 0, 5);
+	        analysisView.add(contingent, 0, 6);
+	        analysisView.add(natural, 0, 7);
+	        analysisView.add(attempts, 0, 8);
 	        
 	        TextField text1 = new TextField("Grade");
-	        grid.add(text1, 1, 0);
+	        analysisView.add(text1, 1, 0);
 	        TextField text2 = new TextField("Grade");
-	        grid.add(text2, 1, 1);
+	        analysisView.add(text2, 1, 1);
 	        TextField text3 = new TextField("Grade");
-	        grid.add(text3, 1, 2);
+	        analysisView.add(text3, 1, 2);
 	        TextField text4 = new TextField("Grade");
-	        grid.add(text4, 1, 3);
+	        analysisView.add(text4, 1, 3);
 	        TextField text5 = new TextField("Grade");
-	        grid.add(text5, 1, 4);
+	        analysisView.add(text5, 1, 4);
 	        TextField text6 = new TextField("Grade");
-	        grid.add(text6, 1, 5);
+	        analysisView.add(text6, 1, 5);
 	        TextField text7 = new TextField("Grade");
-	        grid.add(text7, 1, 6);
+	        analysisView.add(text7, 1, 6);
 	        TextField text8 = new TextField("Grade");
-	        grid.add(text8, 1, 7);
+	        analysisView.add(text8, 1, 7);
 	        TextField text9 = new TextField("Grade");
-	        grid.add(text9, 1, 8);
-	        root.setRight(grid);
+	        analysisView.add(text9, 1, 8);
+	        
+	        HBox navigation = new HBox(10);
+	        TextField pos = new TextField(Integer.toString(model.pos));
+	        navigation.getChildren().addAll(new Label("Minute"), pos, new Label("/10"));
+	        analysisView.add(navigation, 0, 9);
+	        Button goLeft = new Button("<");
+	        goLeft.setMaxWidth(50);
+	        Button goRight = new Button(">");
+	        goRight.setMaxWidth(50);
+	        HBox movingControl = new HBox(5);
+	        movingControl.getChildren().addAll(goLeft, goRight);
+	        analysisView.add(movingControl, 1, 9);
+	        root.setRight(analysisView);
 	        
 	        //All code at this point creates the Comments View
-	        GridPane grid2 = new GridPane();
-			grid2.setAlignment(Pos.CENTER);
-	        grid2.setHgap(10);
-	        grid2.setVgap(10);
-	        grid2.setScaleShape(true);
-	        grid2.setPadding(new Insets(25, 25, 25, 25));
+	        GridPane commentsView = new GridPane();
+			commentsView.setAlignment(Pos.CENTER);
+	        commentsView.setHgap(10);
+	        commentsView.setVgap(10);
+	        commentsView.setScaleShape(true);
+	        commentsView.setPadding(new Insets(25, 25, 25, 25));
 	        
 	        ListView<String> list = new ListView<String>();
 	        ObservableList<String> items =FXCollections.observableArrayList (
 	            "Single", "Double", "Suite", "Family App");
 	        list.setItems(items);
-	        grid2.add(list, 0, 0);
+	        commentsView.add(list, 0, 0);
 	        
 	        Button deleteComment = new Button("Delete Comment");
 	        Button printComments = new Button("Print Comments");
 	        HBox buttons = new HBox(10);
 	        buttons.getChildren().addAll(deleteComment, printComments);
 	        buttons.setAlignment(Pos.TOP_LEFT);
-	        grid2.add(buttons, 0, 1);
+	        commentsView.add(buttons, 0, 1);
 	        
 	        TextField fromTime = new TextField("0:00");
 	        TextField toTime = new TextField("0:00");
 	        HBox time = new HBox(10);
 	        time.setAlignment(Pos.TOP_LEFT);
 	        time.getChildren().addAll(new Label("FROM:"), fromTime, new Label("TO:"), toTime);
-	        grid2.add(time, 0, 2);
+	        commentsView.add(time, 0, 2);
 	        
 	        TextArea comments = new TextArea("Write comments here...");
-	        grid2.add(comments, 0, 3);
+	        commentsView.add(comments, 0, 3);
 	        Button add = new Button("Add Comment");
-	        grid2.add(add, 0, 4);
+	        commentsView.add(add, 0, 4);
 	        
-	      //displays the Menu Bar "File" option with all options and hotkeys
+	        //displays the Menu Bar "File" option with all options and hotkeys
 			MenuBar menuBar = new MenuBar();
 			Menu menuFile = new Menu("File");
 			MenuItem openVideo = new MenuItem("Open Video");
@@ -213,7 +200,8 @@ public class Main extends Application {
 				fileChooser.setTitle("Open Video File");
 				File file = fileChooser.showOpenDialog(primaryStage);
 				if(file != null) {
-					model.openVideo(file);
+					root.setLeft(model.openVideo(file, primaryScreenBounds.getWidth(), primaryScreenBounds.getHeight()));
+					
 					close.setDisable(false);
 					saveReview.setDisable(false);
 					saveReviewAs.setDisable(false);
@@ -229,7 +217,7 @@ public class Main extends Application {
 				fileChooser.setTitle("Open Text File");
 				File file = fileChooser.showOpenDialog(primaryStage);
 				if(file != null) {
-					model.openVideo(file);
+					model.openVideo(file, scene.getWidth(), scene.getHeight());
 					close.setDisable(false);
 					saveReview.setDisable(false);
 					saveReviewAs.setDisable(false);
@@ -241,15 +229,33 @@ public class Main extends Application {
 			});
 			
 			analyze.setOnAction(event -> {//change view to show the analysis screen
-				root.setRight(grid);
+				root.setRight(analysisView);
 				analyze.setDisable(true);
 				comment.setDisable(false);
 			});
 			
 			comment.setOnAction(event -> {//change view to show the analysis screen
-				root.setRight(grid2);
+				root.setRight(commentsView);
 				analyze.setDisable(false);
 				comment.setDisable(true);
+			});
+			
+			prev.setOnAction(event -> {
+				model.pos--;
+				model.moveToSection();
+				next.setDisable(false);
+				if(model.pos == 0) {
+					prev.setDisable(true);
+				}
+			});
+			
+			next.setOnAction(event -> {
+				model.pos++;
+				model.moveToSection();
+				prev.setDisable(false);
+				if(model.pos == 9) {
+					next.setDisable(true);
+				}
 			});
 			
 			menuFile.getItems().addAll(openVideo, openReview, saveReview, saveReviewAs, close, quit);
@@ -259,9 +265,13 @@ public class Main extends Application {
 			root.setTop(menuBar);
 	        
 	        //Loads application
+			primaryStage.setX(primaryScreenBounds.getMinX());
+	        primaryStage.setY(primaryScreenBounds.getMinY());
+	        primaryStage.setWidth(primaryScreenBounds.getWidth());
+	        primaryStage.setHeight(primaryScreenBounds.getHeight());
 			primaryStage.setTitle("Austism Video Analyzer");
 			primaryStage.setScene(scene);
-			primaryStage.setResizable(true);
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- TherapistApp/src/application/Model.java
+++ TherapistApp/src/application/Model.java
@@ -2,18 +2,40 @@ package application;
 
 import java.io.File;
 
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
 public class Model {
 	public String videoPath;
 	public String filePath;
+	private Media media;
+	private MediaPlayer mediaPlayer;
+	private MediaView mediaView;
+	private StackPane mediaPane; 
+	private MediaControl control;
+	public int pos;
 	
-	public void openVideo(File file){
+	//opens a video file and has it playing in a media player
+	MediaControl openVideo(File file, double width, double height){
 		try {
 			videoPath = "file://" + file.getCanonicalPath().replace(" ", "%20").replace("\\", "/").replaceAll("^.:", "");
-			//TODO: have video loaded into media player.
+			//Video View
+			
+			mediaPane = new StackPane();
+			mediaPane.setId("mediaPane");
+			media = new Media(videoPath);
+			mediaPlayer = new MediaPlayer(media);
+			mediaPlayer.setAutoPlay(true);
+			control = new MediaControl(mediaPlayer, width, height);
+			pos = 0;
+			return control;
 		}
 		catch (Exception e) {
 			System.out.println(e);
 		}
+		return null;
 	}
 	
 	public void openReview(File file){
@@ -24,5 +46,10 @@ public class Model {
 		catch (Exception e) {
 			System.out.println(e);
 		}
+	}
+	
+	public void moveToSection() {
+		System.out.println(pos);
+		control.goToMinute(pos);
 	}
 }
